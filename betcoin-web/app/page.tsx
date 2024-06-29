fc015f75a5c11fd0ab19a2b1e8b211e8d7a9fa32
Tim <djtimon100@gmail.com>
2024-06-29 21:11:43 +0500

    rebuild style

--- betcoin-web/app/page.tsx
+++ betcoin-web/app/page.tsx
@@ -1,11 +1,12 @@
-"use client"
+"use client";
 
 import { useState } from "react";
 import styles from "./Home.module.css"; // Создайте файл Home.module.css для стилей
+import Image from "next/image";
 
 export default function Home() {
-  const [totalClicks, setTotalClicks] = useState(10);
-  const [curentClick, setCurentClick] = useState(10);
+  const [totalClicks, setTotalClicks] = useState(500);
+  const [curentClick, setCurentClick] = useState(500);
   const [income, setIncome] = useState(0);
 
   const handleClick = () => {
@@ -13,52 +14,53 @@ export default function Home() {
     } else {
       setCurentClick(curentClick - 1);
       setIncome(income + 1);
-      navigator.vibrate(100); 
+      navigator.vibrate(100);
       const ballElement = document.getElementById("ball");
       ballElement?.classList.add(styles.shake);
       setTimeout(() => {
         ballElement?.classList.remove(styles.shake);
-      }, 500); 
+      }, 500);
     }
   };
 
   return (
-    <div className="relative min-h-screen flex flex-col items-center justify-center bg-cover bg-center">
-      {/* Фоновое изображение */}
-      <div
-        className="absolute inset-0 bg-cover bg-center filter blur-sm"
-        style={{
-          backgroundImage: "url('/bg.svg')",
-          transform: "scale(1.3)",
-          filter: "blur(15px)",
-        }}
-      ></div>
-
+    <div className="flex flex-col items-center justify-center min-h-screen">
       {/* Верхний блок */}
       <div className="text-center text-white space-y-5 m-5 z-10 ">
         <button className="bg-gradient-to-r from-yellow-400 to-orange-500 py-2 px-24 rounded font-bold">
           МАГАЗИН
         </button>
         <div className="mt-4 text-xl">
-          <p>ЭНЕРГИЯ: {curentClick}/{totalClicks} (24ч)</p>
+          <p>
+            ЭНЕРГИЯ: {curentClick}/{totalClicks} (24ч)
+          </p>
           <p>БАЛАНС: {income} $BETC</p>
         </div>
       </div>
 
       {/* Блок кликера */}
-      <div className="flex items-center justify-center h-64 w-64 relative">
-        <div
+      <div className="flex items-center justify-center h-64 w-64 m-auto">
+        <Image
+          id="ball"
+          className="w-64 h-64"
+          src={"/ball.svg"}
+          width={300}
+          height={300}
+          alt="ball"
+          onClick={handleClick}
+        ></Image>
+        {/* <div
           id="ball"
           className="absolute inset-0 bg-cover bg-center"
           style={{
             backgroundImage: "url('/ball.svg')",
           }}
           onClick={handleClick}
-        ></div>
+        ></div> */}
       </div>
 
       {/* Нижний блок */}
-      <div className="relative text-center text-white space-y-4 z-10 mt-auto mb-10">
+      <div className="relative text-center text-white space-y-4 z-10 mb-10">
         <button className="bg-gradient-to-r from-yellow-400 to-orange-500 rounded font-bold block w-15">
           ЗАБРАТЬ $BETCOINЫ
         </button>
