4d4541b20b0a8a9bdf22047b283b1b08e9d01f0a
Esensats <esensats@gmail.com>
2024-07-03 22:27:05 +0500

    add placeholder empty reducer to suppress invalid reducer error

--- betcoin-web/src/app/store.ts
+++ betcoin-web/src/app/store.ts
@@ -1,7 +1,9 @@
 import { configureStore } from "@reduxjs/toolkit";
 
+const initialState = {};
+
 export const store = configureStore({
-  reducer: {},
+  reducer: (state = initialState) => state,
 });
 
 export type RootState = ReturnType<typeof store.getState>;
